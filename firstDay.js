b854b409718e8d03d136c2255fb41c770e71019e
Jacob Menke <jamenk@email.wm.edu>
2017-01-14 01:12:02 -0500

    first

---
+++ firstDay.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jacobmenke on 1/11/17.
+ */
+
+//html, css, javascript, java, mysql - full stack developer
+//html, css, javascript, mongodb - pure javascript, relatively fast
+
+//put in semicolons if you are going to minify
+console.log("the answer is " + (2 +2));
+var str = "This is a string.";
+
+console.log(str);
+
+//outside of function var vs novar has no difference
+//inside function has local scope
+
